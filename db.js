3be85eb5657e0bd57067e7bf1ec982fca3b07685
unknown <dzenohaskic89@gmail.com>
2022-01-24 22:08:58 +0100

    accounts

---
+++ db.js
@@ -0,0 +1,26 @@
+let db = [
+    {
+        id : 1,
+        name : "danilo",
+        lastname : "vesovic",
+        email : "danilo@gmail.com",
+        phone : "11-11-11-111"
+
+    },
+    {
+        id : 2,
+        name : "mirko",
+        lastname : "mirkovic",
+        email : "mirko@gmail.com",
+        phone : "22-22-22-222"
+
+    },
+    {
+        id : 3,
+        name : "dzeno",
+        lastname : "haskic",
+        email : "dzenohaskic89@gmail.com",
+        phone : "33-33-333"
+
+    }
+];
