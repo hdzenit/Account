b647e49c60466db37e2a244c4752253273617c55
unknown <dzenohaskic89@gmail.com>
2022-01-25 21:58:38 +0100

    account II

--- main.js
+++ main.js
@@ -1,4 +1,24 @@
 let accountsTableBody = document.querySelector("#accounts-table-body");
+let allLinks = document.querySelectorAll('.nav-link');
+let accountsView = document.querySelector('#accounts-view');
+let addAccountView = document.querySelector('#add-account-view');
+let views = document.querySelectorAll('.view');
+
+for (let i = 0; i < allLinks.length; i++) {
+    allLinks[i].addEventListener('click', showView);
+    
+}
+
+function showView(e){
+    e.preventDefault();
+    for (let i = 0; i < views.length; i++) {
+        views[i].style.display = "none";
+        
+    }
+    let id = `#${this.getAttribute("href")}`;
+    document.querySelector(id).style.display = "block";
+}
+
 
 createAccountsTable();
 
